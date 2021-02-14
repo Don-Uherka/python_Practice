aae71a25715399b12de43516b6f577d91bbaa4dc
Don-Uherka <uherkadon@yahoo.com>
2021-02-14 11:42:16 -0600

    This part of the course was going over variables in python and how to shorten code and make it look cleaner

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-print("Hello World please have this work")
+students_count = 1000
+rating = 4.99
+is_published = False
+course_name = """
+Multiple 
+Lines
+"""
+
+x = 1
+y = 2
+x,y = 1, 2
+
+x = y = 1
