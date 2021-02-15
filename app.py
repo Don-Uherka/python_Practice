f3159aaf8a32f77c18bc204c571853c935a4defa
Don-Uherka <uherkadon@yahoo.com>
2021-02-14 18:26:42 -0600

    this is practicing taking a string and defining what index you would want

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
-age: int = 20
-age = "Python"
-print(age)
+course = "Python Programming"
+print(len(course))
+print(course[0])
+print(course[-1])
+# this will give back the letter at the very end of the string, all negative numbers will start at the end of the string
+
+
+print(course[0:3])
+# this will return the indexs until the 3 index so it returns Pyt
+print(course[:3])  # Python will automatically start at the 0 index
+
+print(course[0:])
+# if you do not define the end index Python will stop at the end of the string
