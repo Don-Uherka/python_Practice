baccc726dd4d888449de051b68149c9ce0bd013b
Don-Uherka <uherkadon@yahoo.com>
2021-03-08 22:11:17 -0600

    in this section of the course it goes over the different ways to use a for loop to your advantage

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-import math
+   for x in "Python":
+        print(x)
 
-print(math.floor(PI))
+    for x in ['a', 'b', 'c']:
+        print(x)
 
+    for x in range(5)  # when calling range and 5 you will get numbers 0-4 
 
-PI = 3.14
+    for x in range(0, 10, 2)  # when calling this it will give you the numbers 0-10 in increments of 2
 
-
-print(round(PI))
-print(abs(PI))
+# range objects are iterable like string and lists giving us the capablity to use them in for loops
