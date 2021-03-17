60970cd039995a7cb62ed7f17b8960d5ad2aa6e4
Don-Uherka <uherkadon@yahoo.com>
2021-03-16 20:52:01 -0500

    This portion of the tutorial gives a brief demo on For Else loops

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
-   for x in "Python":
-        print(x)
-
-    for x in ['a', 'b', 'c']:
-        print(x)
-
-    for x in range(5)  # when calling range and 5 you will get numbers 0-4 
-
-    for x in range(0, 10, 2)  # when calling this it will give you the numbers 0-10 in increments of 2
-
-# range objects are iterable like string and lists giving us the capablity to use them in for loops
+names = ["John", "Mary"]
+for name in names:
+    if name.startswith("J"):
+        print("Found")
+        break
+else:
+    print("Not found")
