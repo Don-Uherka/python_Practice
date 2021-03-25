2ed1cb59968bdac6bf5ee21ac5952837a6f1c6f4
Don-Uherka <uherkadon@yahoo.com>
2021-03-24 21:18:59 -0500

    these are examples of lists in Python and what functions might be useful

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-def save_user(**user)
+letters = ["a", "b", "c"]
+matrix = [[0, 1], [2, 3]]
+zeros = [0] * 5
+numbers = list(range(20))  # this will create a list of numbers from 0-20
+# this will give you each individual chars in the string as a list
+chars = list("Hello World")
+combined = zeros + letters
 
-
-print(user["id"])
-
-
-save_user(id=1, name="admin")
+# when you print and use the length function (len) with a list it will give you the length of that list
