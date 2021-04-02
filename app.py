6a7dd9486debb9d4b7c0818e50ba17a5e72418a2
Don-Uherka <uherkadon@yahoo.com>
2021-04-02 14:29:46 -0500

    this is going over unpacking a list to get the items that you want out of a list without taking everything out of the list

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
-letters = ["a", "b", "c", "d"]
-letters[0] = "A"
-print(letters[0:3])
-print(letters[::2])  # this will skip the second char in the list
-
-
-numbers = list(range(20))
-print(numbers[::-1])  # this will return the list in reverse order
+numbers = [1, 2, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
+first, second, third, *other = numbers
+print(first)
+print(other)
+# you can put *other anywhere you need it to go and pull just the things out of the list that you want
